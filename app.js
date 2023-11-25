5fd9fb3af4a14ef1579261d7d5fb552ce109151e
JJ <jonjones320@gmail.com>
2023-11-24 22:01:34 -0800

    modified random color html, js, & css

---
+++ app.js
@@ -0,0 +1,20 @@
+function randomColor () {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+// const h1 = document.querySelector('h1');
+// setInterval(function() {h1.style.color = randomColor()}, 500)
+
+const letters = document.querySelectorAll('.letter');
+
+
+
+const intervalId = setInterval(function () {
+    for (let letter of letters) {
+        letter.style.color = randomColor();
+    }
+}
+,500)
